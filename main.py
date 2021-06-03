f9ef32feba15f7549c35959982ebe794e15705cc
Muhammed Didin <mdidin80@gmail.com>
2021-06-03 22:13:36 +0300

    Read from csv files implemented

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import numpy as np
+import csv
+
+#reading data from csv files.
+with open('IrisGeometicFeatures_TrainingSet.txt') as csv_file:
+    geomatic_training = csv.reader(csv_file, delimiter=',')
+
+
+with open('IrisGeometicFeatures_TestingSet.txt') as csv_file:
+    geomatic_test = csv.reader(csv_file, delimiter=',')
+
+
+with open('IrisTextureFeatures_TrainingSet.txt') as csv_file:
+    texture_training = csv.reader(csv_file, delimiter=',')
+
+
+with open('IrisTextureFeatures_TestingSet.txt') as csv_file:
+    texture_test = csv.reader(csv_file, delimiter=',')
